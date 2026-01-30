41d249aa93f75b8d7d11a92e8aa3684e0064a13a
adityatech084-arch <adityatech084@gmail.com>
2026-01-30 19:59:44 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api/v1"   ,
+    baseURL: import.meta.env.VITE_API_BASE_URL   ,
     withCredentials: true,          
 });
